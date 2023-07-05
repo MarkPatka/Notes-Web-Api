85a1335006e4bb488fc55e8021d72e846b433ae3
usr <usr@PC1>
2023-07-05 15:35:21 +0300

    Add project files.

---
+++ Notes.Persistance/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Notes.Application.Interfaces;
+using System.Reflection;
+
+namespace Notes.Persistance;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration["DbConnection"];
+
+        services.AddDbContext<NotesDbContext>(options =>
+        {
+            options.UseSqlite(connectionString);
+        });
+
+        services.AddScoped<INotesDbContext>(provider => 
+            provider.GetService<NotesDbContext>() ?? throw new ArgumentNullException(nameof(NotesDbContext)));
+
+        services.AddMediatR(cfg => 
+        { 
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()); 
+        });
+
+        return services;
+    }
+}
